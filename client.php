040cb8143eb1cf9ffec7fef7f9a346012190cd22
Peter Petermann <peter.petermann@gameforge.com>
2014-11-02 00:41:47 +0100

    added magic client examples

--- client.php
+++ client.php
@@ -13,8 +13,25 @@ echo $client->invokeRpcCall("sayHello",[]) . "\n";
 // hello user! (first param == user)
 echo $client->invokeRpcCall("sayHello",["user"]) . "\n";
 
+
+// lets fetch an exception..
 try {
     $client->invokeRpcCall("throwException");
 } catch (\Devedge\XmlRpc\Client\RemoteException $e) {
     echo "caught exception: (" . $e->getMessage() .")\n";
 }
+
+// alright, tunk invokeRpcCall is ugly, and there might be a nicer solution? You are Right,
+// use MagicClient instead of Client
+$client = new \Devedge\XmlRpc\MagicClient("http://localhost:8100/server.php", 'example');
+
+// magic client allows you to call the methods on your servers service by using the magic __call method.
+echo $client->sayHello("magic") . "\n";
+
+// a disadvantage of this is that your IDE probably marks "sayHello" as unknown method. The simple solution
+// for that is to extend MagicClient with an own class (in this example, its the ExampleMagic class),
+// and annotate the method there (at least that works for PhpStorm, not sure about all other IDE.
+$client = new \Devedge\Example\Client\ExampleMagic("http://localhost:8100/server.php", 'example');
+
+// this should not be marked as missing method
+echo $client->sayHello("my magic");
---
+++ src/Devedge/Example/Client/ExampleMagic.php
@@ -0,0 +1,14 @@
+<?php
+namespace Devedge\Example\Client;
+
+use Devedge\XmlRpc\MagicClient;
+
+/**
+ * Class ExampleMagic
+ *
+ * @package Devedge\Example\Client
+ * @method string sayHello(string $name)
+ */
+class ExampleMagic extends MagicClient
+{
+}
